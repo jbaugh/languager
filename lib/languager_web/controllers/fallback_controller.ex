a641af8408b2c9833c3de85401d49349b4054636
Jarrett Baugh <jarrett.baugh@gmail.com>
2020-03-13 22:28:30 -0700

    Fix fallback controller

--- lib/languager_web/controllers/fallback_controller.ex
+++ lib/languager_web/controllers/fallback_controller.ex
@@ -6,6 +6,20 @@ defmodule LanguagerWeb.FallbackController do
   """
   use LanguagerWeb, :controller
 
+   def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
+    conn
+    |> put_status(:unprocessable_entity)
+    |> put_view(LanguagerWeb.ChangesetView)
+    |> render("error.json", changeset: changeset)
+  end
+
+  def call(conn, {:error, :unauthorized}) do
+    conn
+    |> put_status(:unauthorized)
+    |> put_view(LanguagerWeb.ErrorView)
+    |> render("auth_required.json")
+  end
+
   def call(conn, {:error, :not_found}) do
     conn
     |> put_status(:not_found)
